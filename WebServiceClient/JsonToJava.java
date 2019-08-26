542472ae3c477c67101e6e1bd1d5334a05793aed
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-08-26 14:16:09 +0300

    Update JsonToJava.java

--- WebServiceClient/JsonToJava.java
+++ WebServiceClient/JsonToJava.java
@@ -3,8 +3,9 @@
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.create();
+        // the next line fo testing (you will have the real response)
         String serverResponse = "{\"temp\": 280.32,\"pressure\": 1012,\"humidity\": 81,\"temp_min\": 279.15,\"temp_max\": 281.15}";
-        
+        MainWeather mw = gson.fromJson(serverResponse, MainWeather.class);
         
 public class MainWeather {
 
